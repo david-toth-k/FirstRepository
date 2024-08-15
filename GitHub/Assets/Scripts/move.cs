cacef9ca8380f4f85ed788cd1a55ac842dc8cf0b
david-toth-k <wortexsportex@gmail.com>
2024-08-15 16:58:58 -0400

    move

---
+++ GitHub/Assets/Scripts/move.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Debug.Log(horizontalInput);
+        float verticalInput = Input.GetAxis("Vertical");
+        Debug.Log(verticalInput);
+        transform.position = transform.position + new Vector3(horizontalInput, 0, verticalInput) * Time.deltaTime;
+    }
+}
